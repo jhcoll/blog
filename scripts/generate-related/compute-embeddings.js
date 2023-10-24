8768f691f9babaca6eb27db501e67d4a14ee5188
jcollier <jcollier@scottlogic.com>
2023-10-24 15:01:01 +0100

    Update configs

--- scripts/generate-related/compute-embeddings.js
+++ scripts/generate-related/compute-embeddings.js
@@ -3,20 +3,47 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const { markdownToTxt } = require("markdown-to-txt");
 
-const summarisePost = async (post) => {
-  const OPENAI_API_KEY = process.env.npm_config_openai_api_key;
+
+(async () => {
+  const outputPath = './scripts/generate-related/data';
+  if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath);
+  
+  const paths = await glob('./_posts/**/20{[1][3-9],[2][0-3]}-*.{md,markdown,html}').then((paths) => {
+    return paths;
+  });
+
+  for (const path of paths) {
+    const file = path.split("/").pop();
+    const filename = `${outputPath}/${file}`;
+
+    if (!fs.existsSync(filename)) {
+      console.log("embedding", filename)
+      await summarisePost(formatContent(path)).then((embedding) => {
+        fs.writeFileSync(filename, JSON.stringify(embedding, null, 2));
+      }).catch((err) => {
+        console.log("failed to embed: ", filename);
+        console.log(err);
+      });
+    } else {
+      console.log("skipping: ", filename);
+    }
+  }
+})();
+
+const formatContent = (post) => {
   const file = fs.readFileSync(post, "utf8");
-  // remove front-matter
   const body = file.split("---")[2];
-  // convert from markdown to text
   let text = markdownToTxt(body);
   text = text.replace(
     /\{% highlight [a-zA-Z]* %\}[\s\S]*\{% endhighlight %\}/g,
     " "
   );
   // remove whitespace and preserve ~2048 tokens (approx 1000 words)
-  const words = text.split(/[\s]+/);
-  const data = words.slice(0, 1000).join(" ");
+  return text.split(/[\s]+/).slice(0, 1000).join(" ");
+};
+
+const summarisePost = async (data) => {
+  const OPENAI_API_KEY = process.env.npm_config_openai_api_key;
 
   return await fetch(
     "https://api.openai.com/v1/engines/babbage-similarity/embeddings",
@@ -29,11 +56,10 @@ const summarisePost = async (post) => {
       body: JSON.stringify({
         input: data,
       }),
-    }
-  )
+    })
     .then((res) => {
       if(res.status !== 200) {
-        console.log(res.status, res.statusText)
+        throw new Error(res.statusText);
       }
       return res.json()
     })
@@ -44,30 +70,4 @@ const summarisePost = async (post) => {
         return [];
       }
     });
-
-  // exit process
 };
-
-(async () => {
-  const paths = await glob('./_posts/**/{20[1][3-9],20[2][0-3]}-*.{md,markdown,html}').then((paths) => {
-    return paths;
-  });
-
-  if (!fs.existsSync("./scripts/generate-related/data")) {
-    fs.mkdirSync("./scripts/generate-related/data");
-  }
-
-  for (const path of paths) {
-    const file = path.split("/").pop();
-    const filename = `./scripts/generate-related/data/${file}`;
-
-    if (!fs.existsSync(filename)) {
-      await summarisePost(path).then((embedding) => {
-        fs.writeFileSync(filename, JSON.stringify(embedding, null, 2));
-      });
-      console.log("embedding", filename);
-    } else {
-      console.log("skipping", filename);
-    }
-  }
-})();
