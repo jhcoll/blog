884f878b5d47af79666a9664a8b7b715fb9e112c
jcollier <jcollier@scottlogic.com>
2023-11-09 16:58:18 +0000

    add logging

--- scripts/images/resize-images.js
+++ scripts/images/resize-images.js
@@ -9,11 +9,18 @@ const fs = require("fs");
     });
 
     for (const path of paths) {
-        const { data, info } = await sharp(path)
-        .resize(1140, 1140, {fit: 'inside', withoutEnlargement: true})
-        .toBuffer({ resolveWithObject: true });
+        try{
+            console.log('Resizing image: ', path);
+            const { data, info } = await sharp(path)
+            .resize(1140, 1140, {fit: 'inside', withoutEnlargement: true})
+            .toBuffer({ resolveWithObject: true });
 
+            console.log('Size after resize: width - ', info.width, ', height - ', info.height, ', size - ', info.size);
 
-        await fs.promises.writeFile(path, data); 
+            await fs.promises.writeFile(path, data); 
+        } catch (err) {
+            console.log('failed to resize image: ', path);
+            throw err;
+        }
     }
 })();
